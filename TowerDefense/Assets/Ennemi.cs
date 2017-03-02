c9737f6fb2df5abb6b26c7189741b63df4fff8e4
nathan marchand <nathan marchand>
2017-03-02 20:03:49 +0100

    first

---
+++ TowerDefense/Assets/Ennemi.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemi : MonoBehaviour {
+    public Rigidbody2D body;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        body.position += new Vector2(0, 0.1f);
+    }
+}
